6d0a58f5373d4dd460ba8246010e383bccb502ca
fanglingxiao <fanglingxiao@guoquan.cn>
2021-10-14 13:56:39 +0800

    ##update

--- src/juc/start/Test02_HowToCreateThread.java
+++ src/juc/start/Test02_HowToCreateThread.java
@@ -1,12 +1,16 @@
 package juc.start;
 
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author fanglingxiao
  * @version 1.0
  * @description 如何去创建线程
- * 三种方式：1继承Tread接口 2实现Runnable接口 3线程池
+ * 三种方式：1继承Tread类 2实现Runnable接口 3线程池
  * @date 2021/10/9 3:10 下午
  **/
+@Slf4j
 public class Test02_HowToCreateThread {
     /**
      * 1.继承Tread类
@@ -14,7 +18,7 @@ public class Test02_HowToCreateThread {
     private static class MyThread extends Thread {
         @Override
         public void run() {
-            System.out.println("Hello MyThread!");
+            log.info("Extends Thread");
         }
     }
 
@@ -24,14 +28,16 @@ public class Test02_HowToCreateThread {
     private static class MyRun implements Runnable {
         @Override
         public void run() {
-            System.out.println("Hello MyRun!");
+            log.info("Hello MyRun!");
         }
     }
 
     public static void main(String[] args) {
-        new MyThread().start();
-        new Thread(new MyRun()).start();
+        MyThread t = new MyThread();
+        t.setName("Extends Thread");
+        t.start();
+        new Thread(new MyRun(),"Runnable").start();
         // 匿名类部类实现 实际也只重写Runnable接口
-        new Thread(() -> System.out.println("Hello MyRun Lambda!")).start();
+        new Thread(() -> log.info("Hello MyRun Lambda!"),"Lambda").start();
     }
 }
